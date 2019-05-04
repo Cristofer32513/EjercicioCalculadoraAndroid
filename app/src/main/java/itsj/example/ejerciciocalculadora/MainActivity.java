ad0ba2484f02b08a8f4b8f39bae47e3a96dd6761
Cristofer32513 <cristofer32513@gmail.com>
2019-05-03 22:36:56 -0500

    Se declararon las variables que se utilizarian a lo largo del proyecto y se instanciaron en el metodo onCreate.

--- app/src/main/java/itsj/example/ejerciciocalculadora/MainActivity.java
+++ app/src/main/java/itsj/example/ejerciciocalculadora/MainActivity.java
@@ -2,12 +2,105 @@ package itsj.example.ejerciciocalculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText cajaHistorial, cajaEntrada;
+    Button btnMC, btnMR, btnMMas, btnMMenos, btnMS, btnMHistorial;
+    Button btnPorcentaje, btnRaiz, btnPotencia, btnUnoEntreX, btnDividir, btnMultiplicar,  btnRestar, btnSumar, btnIgual,
+            btnCE, btnC, btnDelete, btnMasMenos, btnPunto;
+    Button btnUno, btnDos, btnTres, btnCuatro, btnCinco, btnSeis, btnSiete, btnOcho, btnNueve, btnCero;
+
+
+    double primerNumero, segundoNumero, resultado;
+    public int operacionARealizar;
+    double ms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        cajaEntrada=findViewById(R.id.caja_entrada);
+        cajaHistorial=findViewById(R.id.caja_historial);
+        operacionARealizar=0;
+
+
+        btnMC=findViewById(R.id.btn_mc);
+        btnMC.setOnClickListener(this);
+        btnMR=findViewById(R.id.btn_mr);
+        btnMR.setOnClickListener(this);
+        btnMMas=findViewById(R.id.btn_m_mas);
+        btnMMas.setOnClickListener(this);
+        btnMMenos=findViewById(R.id.btn_m_menos);
+        btnMMenos.setOnClickListener(this);
+        btnMS=findViewById(R.id.btn_ms);
+        btnMS.setOnClickListener(this);
+        btnMHistorial=findViewById(R.id.btn_m_historial);
+        btnMHistorial.setOnClickListener(this);
+        btnMultiplicar=findViewById(R.id.btn_multiplicar);
+        btnMultiplicar.setOnClickListener(this);
+        btnSumar=findViewById(R.id.btn_sumar);
+        btnSumar.setOnClickListener(this);
+
+
+        btnPorcentaje=findViewById(R.id.btn_porcentaje);
+        btnPorcentaje.setOnClickListener(this);
+        btnRaiz=findViewById(R.id.btn_raiz);
+        btnRaiz.setOnClickListener(this);
+        btnPotencia=findViewById(R.id.btn_potencia);
+        btnPotencia.setOnClickListener(this);
+        btnUnoEntreX=findViewById(R.id.btn_uno_entre_x);
+        btnUnoEntreX.setOnClickListener(this);
+        btnDividir=findViewById(R.id.btn_dividir);
+        btnDividir.setOnClickListener(this);
+        btnRestar=findViewById(R.id.btn_restar);
+        btnRestar.setOnClickListener(this);
+        btnIgual=findViewById(R.id.btn_igual);
+        btnIgual.setOnClickListener(this);
+        btnCE=findViewById(R.id.btn_ce);
+        btnCE.setOnClickListener(this);
+        btnC=findViewById(R.id.btn_c);
+        btnC.setOnClickListener(this);
+        btnDelete=findViewById(R.id.btn_delete);
+        btnDelete.setOnClickListener(this);
+        btnMasMenos=findViewById(R.id.btn_mas_menos);
+        btnMasMenos.setOnClickListener(this);
+        btnPunto=findViewById(R.id.btn_punto);
+        btnPunto.setOnClickListener(this);
+
+
+        btnUno=findViewById(R.id.btn_1);
+        btnUno.setOnClickListener(this);
+        btnDos=findViewById(R.id.btn_2);
+        btnDos.setOnClickListener(this);
+        btnTres=findViewById(R.id.btn_3);
+        btnTres.setOnClickListener(this);
+        btnCuatro=findViewById(R.id.btn_4);
+        btnCuatro.setOnClickListener(this);
+        btnCinco=findViewById(R.id.btn_5);
+        btnCinco.setOnClickListener(this);
+        btnSeis=findViewById(R.id.btn_6);
+        btnSeis.setOnClickListener(this);
+        btnSiete=findViewById(R.id.btn_7);
+        btnSiete.setOnClickListener(this);
+        btnOcho=findViewById(R.id.btn_8);
+        btnOcho.setOnClickListener(this);
+        btnNueve=findViewById(R.id.btn_9);
+        btnNueve.setOnClickListener(this);
+        btnCero=findViewById(R.id.btn_0);
+        btnCero.setOnClickListener(this);
+    }
+
+
+
+
+    @Override
+    public void onClick(View v) {
+        
     }
 }
